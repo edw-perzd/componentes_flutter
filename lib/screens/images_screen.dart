d9e8ac635741abec47d18b12afe5040e9eda5ce4
Eduardo <legna246.dav@gmail.com>
2024-03-06 14:01:35 -0600

    17 Images 2

--- lib/screens/images_screen.dart
+++ lib/screens/images_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:practica3_componentes/themes/app_theme.dart';
 
 class ImagesScreen extends StatefulWidget {
   const ImagesScreen({super.key});
@@ -17,19 +18,38 @@ class _ImagesScreenState extends State<ImagesScreen> {
         ),
       ),
       body: ListView(
-        children: [
-          imageCard(),
-        ],
+        children: [imageCard(), imageWeb()],
       ),
     );
   }
 
   Card imageCard() {
     return Card(
-      shape: RoundedRectangleBorder(
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
+      margin: const EdgeInsets.all(20),
+      elevation: 10,
+      child: ClipRRect(
         borderRadius: BorderRadius.circular(30),
+        child: Column(
+          children: [
+            const Image(image: AssetImage('assets/imgs/BMW_image.jpg')),
+            Container(
+              padding: const EdgeInsets.all(10),
+              child: Text(
+                'Marca BMW',
+                style: AppTheme.lightTheme.textTheme.bodySmall,
+              ),
+            )
+          ],
+        ),
       ),
-      child: const Image(image: AssetImage('assets/imgs/BMW_image.jpg')),
+    );
+  }
+
+  Widget imageWeb() {
+    return Center(
+      child: Image.network(
+          'https://media.istockphoto.com/id/1127302688/es/vector/icono-de-pato-amarillo.jpg?s=612x612&w=0&k=20&c=qbHH2q0eR_8H_XFFAn-NJi5WINYtcN2OrruQ1KCG0W4='),
     );
   }
 }
